38dc6aa4a01c329ea308bc639c4c84e1e3316d1d
Lucas Antunes <lucasdanielantunes@gmail.com>
2022-03-31 23:16:51 -0300

    Avançando com Controladores

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -19,6 +19,8 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
+
 Route::get('/login', function(){
     return 'Login';
 })->name('site.login');
