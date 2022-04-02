60d0e57a0d80a6236a3909bc33ee814f22d2f3d5
Lucas Antunes <lucasdanielantunes@gmail.com>
2022-04-02 12:34:00 -0300

    Atualização do projeto

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -17,14 +17,26 @@ class ContatoController extends Controller
     public function salvar(Request $request){
 
         //Realizar a validação dos dados
-        $request->validate([
+        $regras = [
             'nome' => 'required|min:3|max:40',
             'telefone' => 'required',
-            'email' => 'required',
-            'motivo_contato' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
-        ]);
+        ];
 
-        //SiteContato::create($request->all());
+        $feedback =[
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'email.email' => 'O e-mail informado não é válido',
+            'required' => 'O campo :attribute deve ser preenchido',
+            'mensagem.max' => 'A mensagem deve ter no máximo 2000 caracteres'
+        ];
+
+        $request->validate($regras, $feedback);
+
+        SiteContato::create($request->all());
+
+        return redirect()->route('site.index');
     }
 }
