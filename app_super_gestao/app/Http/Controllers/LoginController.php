f0e6b1aa57635f2f0651f4c9e6cc881281361a7d
Lucas Antunes <lucasdanielantunes@gmail.com>
2022-04-03 21:08:29 -0300

    Autenticação de usuários

--- app_super_gestao/app/Http/Controllers/LoginController.php
+++ app_super_gestao/app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class LoginController extends Controller
 {
@@ -22,6 +23,50 @@ class LoginController extends Controller
     }
 
     public function autenticar(Request $request){
+        //regras de validação
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
 
+        //as mensagens de feedback de validação
+        $feedback = [
+            'usuario.email' => 'O campo usuário (e-mail) é obrigatório',
+            'senha.required' => 'O campo senha é obrigatório'
+        ];
+
+        //se não passar no validate
+        $request->validate($regras, $feedback);
+
+        //recuperamos os parâmetros do formulário
+        $email = $request->get('usuario');
+        $password = $request->get('senha');
+
+        echo "Usuário: $email | Senha: $password";
+        echo "<br>";
+
+        //iniciar o Model User
+        $user = new User();
+
+        $usuario = $user->where('email', $email)
+                    ->where('password', $password)
+                    ->get()
+                    ->first();
+
+        if(isset($usuario->name)) {
+
+            session_start();
+            $_SESSION['nome'] = $usuario->name;
+            $_SESSION['email'] = $usuario->email;
+
+            return redirect()->route('app.home');
+        } else {
+            return redirect()->route('site.login', ['erro' => 1]);
+        }
+    }
+
+    public function sair() {
+        session_destroy();
+        return redirect()->route('site.index');
     }
 }
