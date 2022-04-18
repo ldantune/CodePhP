ab6c76a8fe0ecc4ece5f1d67faf1a9c7c09dc9fd
Lucas Antunes <lucasdanielantunes@gmail.com>
2022-04-17 22:00:40 -0300

    Verificação de e-mail

--- app_controle_tarefas/routes/web.php
+++ app_controle_tarefas/routes/web.php
@@ -19,10 +19,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('tarefa', 'App\Http\controllers\TarefaController');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+    ->name('home')
+    ->middleware('verified');
+
+Route::resource('tarefa', 'App\Http\controllers\TarefaController')
+    ->middleware('verified');
 
 Route::get('mensagem-teste', function() {
     return new MensagemTesteMail();
