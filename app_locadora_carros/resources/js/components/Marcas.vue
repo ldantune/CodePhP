71226135b5d7769dc44e48ad2d167e9454044d63
Lucas Antunes <lucasdanielantunes@gmail.com>
2022-04-25 23:00:59 -0300

    Cadastro de marca parte 1

---
+++ app_locadora_carros/resources/js/components/Marcas.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <!-- início do card de busca -->
+        <card-component titulo="Busca de marcas">
+          <template v-slot:conteudo>
+            <div class="form-row row">
+              <div class="col mb-3">
+                <input-container-component
+                  titulo="ID"
+                  id="inputId"
+                  id-help="idHelp"
+                  texto-ajuda="Opcional. Informe o ID da marca"
+                >
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="inputId"
+                    aria-describedby="idHelp"
+                    placeholder="ID"
+                  />
+                </input-container-component>
+              </div>
+              <div class="col mb-3">
+                <input-container-component
+                  titulo="Nome da marca"
+                  id="inputNome"
+                  id-help="nomeHelp"
+                  texto-ajuda="Opcional. Informe o nome da marca"
+                >
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="inputNome"
+                    aria-describedby="nomeHelp"
+                    placeholder="Nome da marca"
+                  />
+                </input-container-component>
+              </div>
+            </div>
+          </template>
+
+          <template v-slot:rodape>
+            <button type="submit" class="btn btn-primary btn-sm float-right">
+              Pesquisar
+            </button>
+          </template>
+        </card-component>
+        <!-- fim do card de busca -->
+        <card-component titulo="Relação de marcas">
+          <template v-slot:conteudo>
+            <table-component></table-component>
+          </template>
+
+          <template v-slot:rodape>
+            <button
+              type="button"
+              class="btn btn-primary btn-sm float-right"
+              data-toggle="modal"
+              data-target="#modalMarca"
+            >
+              Adicionar
+            </button>
+          </template>
+        </card-component>
+      </div>
+    </div>
+
+    <!-- início do modal de inclusão de marca -->
+    <modal-component id="modalMarca" titulo="Adicionar marca">
+        <template v-slot:alertas>
+            <alert-component tipo="success" :detalhes="transacaoDetalhes" v-if="transacaoStatus == 'adicionado'"  titulo="Cadastro realizado com sucesso" ></alert-component>
+            <alert-component tipo="danger" :detalhes="transacaoDetalhes" v-if="transacaoStatus == 'erro'" titulo="Erro ao tentar cadastrar a marca" ></alert-component>
+        </template>
+
+      <template v-slot:conteudo>
+        <div class="form-group">
+          <input-container-component
+            titulo="Nome da marca"
+            id="novoNome"
+            id-help="novoNomeHelp"
+            texto-ajuda="Informe o nome da marca"
+          >
+            <input
+              type="text"
+              class="form-control"
+              id="novoNome"
+              aria-describedby="novoNomeHelp"
+              placeholder="Nome da marca"
+              v-model="nomeMarca"
+            />
+          </input-container-component>
+          <!-- {{ nomeMarca }} -->
+        </div>
+
+        <div class="form-group">
+          <input-container-component
+            titulo="Imagem"
+            id="novoImagem"
+            id-help="novoImagemHelp"
+            texto-ajuda="Selecione uma imagem no formato PNG"
+          >
+            <input
+              type="file"
+              class="form-control-file"
+              id="novoImagem"
+              aria-describedby="novoImagemHelp"
+              placeholder="Selecione uma imagem" @change="carregarImagem($event)"
+            />
+          </input-container-component>
+          <!-- {{ arquivoImagem }} -->
+        </div>
+      </template>
+
+      <template v-slot:rodape>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">
+          Fechar
+        </button>
+        <button type="button" class="btn btn-primary" @click="salvar()">Salvar</button>
+      </template>
+    </modal-component>
+    <!-- fim do modal de inclusão de marca -->
+  </div>
+</template>
+<script>
+
+export default {
+
+  data() {
+    return {
+        urlBase: 'http://127.0.0.1:8000/api/v1/marca',
+        nomeMarca: '',
+        arquivoImagem: [],
+        transacaoStatus: '',
+        transacaoDetalhes: {},
+    };
+  },
+  methods: {
+    carregarImagem(e) {
+      this.arquivoImagem = e.target.files;
+    },
+    salvar(){
+        console.log(this.nomeMarca, this.arquivoImagem[0])
+
+        let formData = new FormData();
+        formData.append('nome', this.nomeMarca)
+        formData.append('imagem', this.arquivoImagem[0])
+
+        let config = {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                'Accept' : 'application/json'
+            }
+        }
+
+        axios.post(this.urlBase, formData, config)
+            .then(response => {
+                this.transacaoStatus = 'adicionado'
+                this.transacaoDetalhes = {
+                    mensagem: 'ID do registro: ' + response.data.id
+                }
+            })
+            .catch(errors => {
+                this.transacaoStatus = 'erro'
+                this.transacaoDetalhes = {
+                    mensagem: errors.response.data.message,
+                    dados: errors.response.data.errors
+                }
+                console.log(errors)
+                toaster.show(`Hey! I'm here`);
+            })
+    }
+  },
+};
+</script>
--- app_locadora_carros/resources/views/layouts/app.blade.php
+++ app_locadora_carros/resources/views/layouts/app.blade.php
@@ -26,18 +26,36 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+                    <ul class="navbar-nav mr-auto">
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Clientes</a>
+                            </li>
 
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Locações</a>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Veículos</a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Carros</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="{{ route('marcas') }}">Marcas</a>
+                                    <a class="dropdown-item" href="#">Modelos</a>
+                                </div>
+                            </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
+                    <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
@@ -53,11 +71,11 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -74,6 +92,15 @@
                 </div>
             </div>
         </nav>
+
+        @auth
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">{{Route::currentRouteName()}}</li>
+                </ol>
+            </nav>
+        @endauth
 
         <main class="py-4">
             @yield('content')
